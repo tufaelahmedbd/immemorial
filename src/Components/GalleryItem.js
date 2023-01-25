732ab56eec88c58f8dce00d6ff6c59853b1e8075
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-01-26 01:35:23 +0600

    gallery section's animation added

--- src/Components/GalleryItem.js
+++ src/Components/GalleryItem.js
@@ -1,9 +1,28 @@
+import { useRef } from "react";
+import {
+  useGsapGalleryTitle,
+  useGsapGalleryCategory,
+  useGsapGalleryImage,
+} from "../hooks/gspa";
+
 const GalleryItem = ({ image }) => {
+  const galleryTitleRef = useRef(null);
+  const galleryCategoryRef = useRef(null);
+  const galleryimageRef = useRef(null);
+
+  useGsapGalleryTitle(galleryTitleRef, galleryimageRef);
+  useGsapGalleryCategory(galleryCategoryRef, galleryimageRef);
+  useGsapGalleryImage(galleryimageRef);
   return (
     <div className="gallery-item">
-      <h1 className="gallery-item-title">{image.title}</h1>
-      <p className="gallery-item-category">{image.category}</p>
+      <h1 className="gallery-item-title" ref={galleryTitleRef}>
+        {image.title}
+      </h1>
+      <p className="gallery-item-category" ref={galleryCategoryRef}>
+        {image.category}
+      </p>
       <div
+        ref={galleryimageRef}
         className="gallery-item-img"
         style={{ backgroundImage: `url(${image.src})` }}
       ></div>
